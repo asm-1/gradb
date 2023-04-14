57756fbd5c20a77c965cbb455693d735d62eb7c4
asm-1 <asiman.gasanov.dev@gmail.com>
2023-04-14 20:00:18 +0300

    Update main.cpp
    basic concept of the solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,44 @@
+#include <vector>
+#include <string>
+
+enum data_type
+{
+	Str,
+	Int,
+	Bool,
+	Node_1,
+};
+
+class MetaField
+{
+	public:
+		MetaField(){};
+		~MetaField(){};
+};
+
+class Field{
+	private:
+		data_type type;
+	public:
+		Field(){};
+		~Field(){};
+};
+
+class Node{
+	private:
+		data_type type;
+	public:
+		MetaField meta;
+		std::vector<Field> fields;
+		std::string name;
+
+		Node(){};
+		~Node(){};
+};
+
+
 int main(int argc, char const *argv[])
 {
-	/* code */
+	Node node;
 	return 0;
-}
+};
